72ba4cbc3f711dee98e89d65190ce72e25b9fc0c
nzw3yl <roger.smith@ceryx.com>
2012-09-10 12:42:03 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetingNotes::Application.config.secret_token = 'e6c4290b5aeed2439a3ffd6d20502a4f64ee7d2bc19a1f14f52cd14ee4d917f6aa8e21f54326d6f6ed293aee28f71bc6cefd621efaa1525195a944374394d7c0'
